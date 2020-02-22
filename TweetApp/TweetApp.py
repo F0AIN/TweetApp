35fa3126cdbc91ed11733eac5fe5e08f7d346022
f0ain <FRain7.ag2S00@gmail.com>
2020-02-22 19:16:19 +0900

    [add] tweet ボタンの作成

--- TweetApp/TweetApp.py
+++ TweetApp/TweetApp.py
@@ -1,1 +1,21 @@
+import tkinter as tk
 
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.master = master
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.tweet_button = tk.Button(self)
+        self.tweet_button["text"] = "Tweet !"
+        self.tweet_button["command"] = self.tweet
+        self.tweet_button.pack()
+
+    def tweet(self):
+        print("Tweet !")
+
+root = tk.Tk()
+app = Application(root)
+app.mainloop()
